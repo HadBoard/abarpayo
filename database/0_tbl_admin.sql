55b15cffc8db488aaab1aa26c87620091e22b9fc
--get <sxhadixr@gmail.com>
2021-04-10 08:58:45 +0430

    Update 0_tbl_admin.sql

--- database/0_tbl_admin.sql
+++ database/0_tbl_admin.sql
@@ -3,13 +3,15 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 10, 2021 at 06:15 AM
+-- Generation Time: Apr 10, 2021 at 06:28 AM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 8.0.0
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET
+SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
-SET time_zone = "+00:00";
+SET
+time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -27,26 +29,28 @@ SET time_zone = "+00:00";
 -- Table structure for table `tbl_admin`
 --
 
-CREATE TABLE `tbl_admin` (
-  `id` int(11) NOT NULL,
-  `first_name` varchar(50) NOT NULL,
-  `last_name` varchar(50) NOT NULL,
-  `phone` varchar(20) NOT NULL,
-  `username` varchar(100) NOT NULL,
-  `password` varchar(100) NOT NULL,
-  `last_login` bigint(20) NOT NULL,
-  `created_at` bigint(20) NOT NULL,
-  `updated_at` int(11) NOT NULL,
-  `access` tinyint(1) NOT NULL,
-  `status` int(11) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+CREATE TABLE `tbl_admin`
+(
+    `id`         int(11) NOT NULL,
+    `first_name` varchar(50) CHARACTER SET utf8  NOT NULL,
+    `last_name`  varchar(50) CHARACTER SET utf8  NOT NULL,
+    `phone`      varchar(20) CHARACTER SET utf8  NOT NULL,
+    `username`   varchar(100) CHARACTER SET utf8 NOT NULL,
+    `password`   varchar(100) CHARACTER SET utf8 NOT NULL,
+    `last_login` bigint(20) DEFAULT NULL,
+    `created_at` bigint(20) NOT NULL,
+    `updated_at` int(11) DEFAULT NULL,
+    `access`     tinyint(1) NOT NULL,
+    `status`     int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `tbl_admin`
 --
 
-INSERT INTO `tbl_admin` (`id`, `first_name`, `last_name`, `phone`, `username`, `password`, `last_login`, `created_at`, `updated_at`, `access`, `status`) VALUES
-(1, 'سید هادی', 'رنجبر', '09218248954', 'root', '123456', 1618027274, 1617910804, 1617968287, 1, 1);
+INSERT INTO `tbl_admin` (`id`, `first_name`, `last_name`, `phone`, `username`, `password`, `last_login`, `created_at`,
+                         `updated_at`, `access`, `status`)
+VALUES (1, 'سید هادی', 'رنجبر', '09218248954', 'root', '123456', 1618027274, 1617910804, 1617968287, 1, 1);
 
 --
 -- Indexes for dumped tables
@@ -56,7 +60,7 @@ INSERT INTO `tbl_admin` (`id`, `first_name`, `last_name`, `phone`, `username`, `
 -- Indexes for table `tbl_admin`
 --
 ALTER TABLE `tbl_admin`
-  ADD PRIMARY KEY (`id`),
+    ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `username` (`username`);
 
 --
@@ -67,7 +71,7 @@ ALTER TABLE `tbl_admin`
 -- AUTO_INCREMENT for table `tbl_admin`
 --
 ALTER TABLE `tbl_admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+    MODIFY `id` int (11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
