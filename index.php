042193211edfe3aba8eff69e6c09088234c8a307
karimif1999 <karimifatemeh1@yahoo.com>
2021-04-11 13:44:55 +0430

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fa" dir="rtl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ابرپایو</title>
+
+
+    <link rel="stylesheet" href="assets/css/style.css">k
+    <link rel="stylesheet" href="assets/css/fontiran.css">k
+</head>
+<body>
+    
+</body>
+</html>
