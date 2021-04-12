52dedc63f61c5140202790e51e7701cbc708b25d
--get <sxhadixr@gmail.com>
2021-04-12 12:23:01 +0430

    Update footer.php

---
+++ footer.php
@@ -0,0 +1,186 @@
+<!-- eof stores -->
+<footer>
+    <div class="newsletter">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 download_app">
+                    <h5>دیافت اپلیکیشن</h5>
+                    <button class="main_btn middle_btn">
+                        <i class="fa fa-android"></i>
+                        دانلود نسخه اندروید
+                    </button>
+                </div>
+                <div class="col-md-6">
+                    <h3>اشتراک خبرنامه</h3>
+                    <p>جهت اطلاع از آخرین تخفیف های شهرستان ، آدرس ایمیل خود را وارد کنید و در خبرنامه مشترک شوید.</p>
+                    <div class="search_header email_footer">
+                        <input placeholder="ایمیل خود را وارد کنید .">
+                        <button><span >
+                              اشتراک
+                              </span></button>
+                    </div>
+                </div>
+                <div class="col-md-3 namad_footer">
+                    <div class="namad_cell">
+                        <img src="assets/images/samandehi@2x.png">
+                    </div>
+                    <div class="namad_cell">
+                        <img src="assets/images/samandehi@2x.png">
+                    </div>
+                </div>
+            </div>
+        </div><!-- eof newsLetter -->
+    </div>
+    <div class="main_footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-9">
+                    <div class="menu_footer">
+                        <ul>
+                            <h4>ابرپایو</h4>
+                            <li>
+                                <a>درباره ابرپایو</a>
+                            </li>
+                            <li>
+                                <a>قوانین و مقررات</a>
+                            </li>
+                            <li>
+                                <a>حریم خصوصی</a>
+                            </li>
+                            <li>
+                                <a>ژتون در رسانه</a>
+                            </li>
+                        </ul>
+                        <ul>
+                            <h4>ابرپایو</h4>
+                            <li>
+                                <a>پاسخ به پرسش های متدوال</a>
+                            </li>
+                            <li>
+                                <a>قوانین و مقررات</a>
+                            </li>
+                            <li>
+                                <a>حریم خصوصی</a>
+                            </li>
+                            <li>
+                                <a>ژتون در رسانه</a>
+                            </li>
+                        </ul>
+                        <ul>
+                            <h4>ابرپایو</h4>
+                            <li>
+                                <a>درباره ابرپایو</a>
+                            </li>
+                            <li>
+                                <a>قوانین و مقررات</a>
+                            </li>
+                            <li>
+                                <a>حریم خصوصی</a>
+                            </li>
+                            <li>
+                                <a>ژتون در رسانه</a>
+                            </li>
+                        </ul>
+                        <ul>
+                            <h4>ابرپایو</h4>
+                            <li>
+                                <a>درباره ابرپایو</a>
+                            </li>
+                            <li>
+                                <a>قوانین و مقررات</a>
+                            </li>
+                            <li>
+                                <a>حریم خصوصی</a>
+                            </li>
+                            <li>
+                                <a>ژتون در رسانه</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="contact_footer">
+                        <div class="row">
+                            <a class="main_btn">
+                                <p>شماره تماس : </p>
+                                <span>
+                                            035 - 8256589
+                                        </span>
+                            </a>
+                        </div>
+                        <div class="row">
+                            <div class="socila_icons">
+                                <div>
+                                    <a href="#">
+                                        <i class="fa fa-user"></i>
+                                    </a>
+                                </div>
+                                <div>
+                                    <a href="#">
+                                        <i class="fa fa-user"></i>
+                                    </a>
+                                </div>
+                                <div>
+                                    <a href="#">
+                                        <i class="fa fa-user"></i>
+                                    </a>
+                                </div>
+                                <div>
+                                    <a href="#">
+                                        <i class="fa fa-user"></i>
+                                    </a>
+                                </div>
+                                <div class="socila_icons_availabel">
+                                    <a  href="#">
+                                        <i class="fa fa-user"></i>
+                                    </a>
+                                </div>
+                                <div>
+                                    <a href="#">
+                                        <i class="fa fa-user"></i>
+                                    </a>
+                                </div>
+                                <div>
+                                    <a href="#">
+                                        <i class="fa fa-user"></i>
+                                    </a>
+                                </div>
+
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+    <!-- eof main footer -->
+    <div class="copyright">
+        <div class="container">
+            <p>کلیه حقوق این سایت متعلق به شرکت حامی تک میبا شد . &copy copyright</p>
+        </div>
+    </div>
+</footer>
+
+
+<script>
+    let tab_btns = document.querySelectorAll('.tablinks')
+    let tab_content = document.querySelectorAll('.tabcontent')
+    tab_content[0].style.display='block';
+    for (let index = 0; index < tab_btns.length; index++) {
+        tab_btns[index].addEventListener('click' , function(){
+            $('.tablinks').removeClass('active_tablink');
+            $('.tabcontent').hide();
+            tab_content[index].style.display = 'block';
+            tab_btns[index].classList.add('active_tablink');
+        })
+
+    }
+
+</script>
+
+</body>
+
+</html>
