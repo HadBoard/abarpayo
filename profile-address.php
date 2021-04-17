24a61999ece0ce21849853ce489ec3a2b6215956
amiri-zakiye <prsv865@gmail.com>
2021-04-17 11:45:06 +0430

    city

--- profile-address.php
+++ profile-address.php
@@ -57,8 +57,7 @@
             ?>
         </select>
         </div>
-        <input name="submit" type="submit" class="main_btn middle_btn " value="ادامه">
-                        
+        <input name="submit" type="submit" class="main_btn middle_btn " value="ادامه">            
     </form>
 </div>
 <script>
--- profile-edit.php
+++ profile-edit.php
@@ -1,4 +1,3 @@
-
 <?
     
     if(isset($_POST['submit'])){
@@ -6,20 +5,37 @@
         $first_name = $action->request('name');
         $last_name = $action->request('lname');
         $national_code = $action->request('national_code');
-        $email = $action->request('email');
-        $result = $action->user_get_phone($phone);
-        $user = $result->fetch_object();
-        if($user->phone == $phone){
-            $command->user_profile_edit($first_name, $last_name,$national_code, $phone, $email);
+        $user_id = $action->user()->id;
+        $user_phone = $user_get($user_id)->phone;
+        if($user_phone == $phone){
+            $command= $action->user_profile_edit($first_name, $last_name,$national_code, $phone,$birthday);
         }else{
             $code=rand(100000,999999);
             // $action->send_sms($phone,$code);
             $action->validation_code_add($user->id,$code);
         }
-        
-
     }
 ?>
+<div id="myModal" class="modal">
+
+<!-- Modal content -->
+<div class="modal-content">
+    <div class="modal-header">
+        <span class="close">×</span>
+        <h2>تایید کد تلفن همراه</h2>
+    </div>
+    <div class="modal-body">
+        <p>کد ارسال شده برای شماره تلفن خود را وارد کنید</p>
+        <input type="text" placeholder="کد ارسال شده">
+        <div class="row">
+            <button name="code_submit" class="main_btn modal_btn">ثبت کد
+            </button>
+        </div>
+    </div>
+  
+</div>
+
+</div>
 <div class="edit_profile_div">
 
     <div class="profile_header">
@@ -34,26 +50,29 @@
     <form action="" method="post">
             <div class="form-group">
                 <label for="name">نام</label>
-                <input type="text" name="name" placeholder="فقط حروف فارسی">
+                <input type="text" name="name" value= "<?= $row->first_name?>" placeholder="فقط حروف فارسی">
             </div>
             <div class="form-group">
                 <label for="lname">نام خانوادگی</label>
-                <input type="text" name="lname" placeholder="فقط حروف فارسی">
+                <input type="text" name="lname" value= "<?= $row->last_name?>" placeholder="فقط حروف فارسی">
             </div>
             <div class="form-group">
                 <label for="national_code">کدملی</label>
-                <input type="text" name="national_code" placeholder="کدملی">
+                <input type="text" name="national_code"  value= "<?= $row->national_code?>" placeholder="کدملی">
             </div>
             <div class="form-group">
                 <label for="phone">شماره موبایل</label>
-                <input type="text" name="phone" placeholder="*******09">
+                <input type="text" name="phone" placeholder="*******09"  value= "<?= $row->phone?>">
             </div>
             <div class="form-group">
-                <label for="email">شماره موبایل</label>
-                <input type="text" name="email" placeholder="example@gmail.com">
+            <label for="birthday">تاریخ تولد</label>
+                <input type="text" id="date" name="birthday" class="form-control"
+                        placeholder="تاریخ تولد"
+                        value="<?= $action->time_to_shamsi($row->birthday) ?>"
+                        required>
             </div>
 
-            <input type="submit" class="main_btn middle_btn " value="ادامه">
+            <input name="submit" type="submit" class="main_btn middle_btn " value="ادامه">
 
         </form>
     </div>
