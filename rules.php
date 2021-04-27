c593ebebcb623a00f78888c75d1a4720bd1e77c9
hadiye <60326153+hadiye@users.noreply.github.com>
2021-04-27 23:11:40 +0330

    Update rules.php

--- rules.php
+++ rules.php
@@ -26,13 +26,36 @@
             <div class="row">
                <div class="rules-p">
                 
-                   <p><?$action->get_system('rules');?></p>
+                   <p><?=$action->get_system('rules');?></p>
                </div>
             </div>
-        
+            <div class="form-group">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" onclick="checkRule()">
+                <label class="form-check-label" for="flexCheckDefault">
+                    پذیرش قوانین
+                </label>
+               <!-- <a href="#" id="rule-btn" class="show-rules">قوانین و مقررات</a>   -->
+        </div>
         </div>
     </div>
 </div>
+
+<script>
+        function checkRule() {
+                document.getElementById("signup").disabled = true;
+                var checkBox = document.getElementById("flexCheckDefault");
+                if (checkBox.checked == true){
+                    document.getElementById("signup").disabled = false;
+                } else {
+                    document.getElementById("signup").disabled = true;
+                }
+            }
+            checkRule()
+
+        document.querySelector('.close-modal').onclick = function(){
+            $('.modal2').hide();
+        }
+    </script>
 <!-- footer -->
 <?include('footer.php')?>
     
