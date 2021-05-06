3225460fa00e3f82d47f7b721c27c2cd27f3f9e1
amiri-zakiye <prsv865@gmail.com>
2021-05-06 17:15:20 +0430

    hum

--- api/api.php
+++ api/api.php
@@ -231,7 +231,8 @@ if(isset($_POST['function'])) {
         }
         $logs = $action->wallet_log_increase($user_id);
         while($log = $logs->fetch_object()){
-            $obj_inner -> wallet_date = $action->time_to_shamsi($action->wallet_log_get_payment($log->payment_id)->date);
+            $payment = $action->wallet_log_get_payment($log->payment_id)->fetch_object();
+            $obj_inner -> wallet_date = $action->time_to_shamsi($payment->date);
             $obj_inner -> amount = $log->amount;
             $obj_inner -> type = 1;
             $obj_inner -> action = 'افزایش موجودی کیف پول';
