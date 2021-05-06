b91ac1f24d898f47b2290bb26f4ed6e5a15e4afe
hadiye <60326153+hadiye@users.noreply.github.com>
2021-05-06 17:43:45 +0330

    Create shop-comment.php

---
+++ guild/shop-comment.php
@@ -0,0 +1,186 @@
+<? require_once "functions/database.php";
+$database = new DB();
+$connection = $database->connect();
+$action = new Action();
+
+// ----------- urls ----------------------------------------------------------------------------------------------------
+// main url for add , edit
+$main_url = "shop-comment.php";
+// main url for remove , change status
+$list_url = "shop-comment-list.php";
+// ----------- urls ----------------------------------------------------------------------------------------------------
+
+// ----------- get data from database when action is edit --------------------------------------------------------------
+$edit = false;
+if (isset($_GET['edit'])) {
+    $edit = true;
+    $id = $action->request('edit');
+    $row = $action->shop_comment_get($id);
+    $replys = $action->shop_comment_reply($id);
+    $reply = $replys->fetch_object();
+}
+// ----------- get data from database when action is edit --------------------------------------------------------------
+
+// ----------- check error ---------------------------------------------------------------------------------------------
+$error = false;
+if (isset($_SESSION['error'])) {
+    $error = true;
+    $error_val = $_SESSION['error'];
+    unset($_SESSION['error']);
+}
+// ----------- check error ---------------------------------------------------------------------------------------------
+
+// ----------- add or edit ---------------------------------------------------------------------------------------------
+if (isset($_POST['submit'])) {
+
+    // get fields
+    $text = $action->request('text');
+    $confirm = $action->request('confirm');
+    $shop_id = $row->shop_id;
+    // send query
+    if ($edit) {
+        if(!empty($text)){
+            $res = $action->shop_comment_list();
+            $user_id=$action->user_get($res->fetch_object()->user_id)->id;
+            $command = $action->shop_comment_add($user_id,$id,$text);
+        }
+        if($confirm){
+            $command1 = $action->shop_comment_confirm($id);
+        }
+    } 
+    // check errors
+    if ($command ||  $command1) {
+        $_SESSION['error'] = 0;
+    } else {
+        $_SESSION['error'] = 1;
+    }
+
+    // bye bye :)
+     header("Location: $main_url?edit=$id");
+
+}
+// ----------- add or edit ---------------------------------------------------------------------------------------------
+
+// ----------- start html :) ------------------------------------------------------------------------------------------
+include('header.php'); ?>
+
+<div class="page-wrapper">
+
+    <div class="row page-titles">
+
+        <!-- ----------- start title --------------------------------------------------------------------------- -->
+        <div class="col-md-12 align-self-center text-right">
+            <?php if (!isset($_GET['action'])) { ?>
+                <h3 class="text-primary">ثبت پاسخ</h3>
+            <?php } else { ?>
+                <h3 class="text-primary">ویرایش پاسخ </h3>
+            <?php } ?>
+        </div>
+        <!-- ----------- end title ----------------------------------------------------------------------------- -->
+
+        <!-- ----------- start breadcrumb ---------------------------------------------------------------------- -->
+        <div class="col-md-12 align-self-center text-right">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item">
+                    <a href="panel.php">
+                        <i class="fa fa-dashboard"></i>
+                        خانه
+                    </a>
+                </li>
+                <li class="breadcrumb-item"><a href=<?= $list_url."?shop=".$row->shop_id ?>> نظرات فروشگاه ها</a></li>
+                <?php if ($edit) { ?>
+                    <li class="breadcrumb-item"><a href="javascript:void(0)">پاسخ</a></li>
+                <?php } else { ?>
+                    <li class="breadcrumb-item"><a href="javascript:void(0)">پاسخ</a></li>
+                <?php } ?>
+            </ol>
+        </div>
+        <!-- ----------- end breadcrumb ------------------------------------------------------------------------ -->
+
+    </div>
+
+    <!-- ----------- start main container ---------------------------------------------------------------------- -->
+    <div class="container-fluid">
+
+        <!-- ----------- start error list ---------------------------------------------------------------------- -->
+        <? if ($error) {
+            if ($error_val) { ?>
+                <div class="alert alert-danger">
+                    عملیات ناموفق بود .
+                </div>
+            <? } else { ?>
+                <div class="alert alert-info text-right">
+                    عملیات موفق بود .
+                </div>
+            <? }
+        } ?>
+        <!-- ----------- end error list ------------------------------------------------------------------------ -->
+
+        <div class="row">
+            <div class="col-lg-6">
+
+                <!-- ----------- start history ----------------------------------------------------------------- -->
+                <? if ($edit) { ?>
+                    <div class="row m-b-0">
+                        <div class="col-lg-6">
+                            <p class="text-right m-b-0">
+                                تاریخ ثبت :
+                                <?= $action->time_to_shamsi($row->created_at) ?>
+                            </p>
+                        </div>
+                        <? if ($row->updated_at) { ?>
+                            <div class="col-lg-6">
+                                <p class="text-right m-b-0">
+                                    آخرین ویرایش :
+                                    <?= $action->time_to_shamsi($row->updated_at) ?>
+                                </p>
+                            </div>
+                        <? } ?>
+                    </div>
+                <? } ?>
+                <!-- ----------- end history ------------------------------------------------------------------- -->
+
+                <!-- ----------- start row of fields ----------------------------------------------------------- -->
+                <div class="card">
+                    <div class="card-body">
+                        <div class="basic-form">
+                            <form action="" method="post">
+                                <div class="form-group">
+                                    <input type="text" class="form-control input-default "
+                                           placeholder="عنوان"
+                                           value="<?= ($edit) ? $row->text : "" ?>" readonly>
+                                </div>
+
+                                <div class="form-group">
+                                    <textarea type="text" name="text" class="form-control input-default " placeholder="متن پاسخ"><?= ($reply) ? $reply->text : "" ?></textarea>
+                                </div>
+                               
+                                <div class="form-actions">
+
+                                    <label class="float-right">
+                                        <input type="checkbox" class="float-right m-1" name="confirm" value="1"
+                                            <? if ($edit && $row->confirm) echo "checked"; ?> >
+                                        تایید نظر کاربر
+                                    </label>
+
+                                    <button type="submit" <?= ( $edit && $row->confirm) ? "disabled" : "" ?> name="submit" class="btn btn-success sweet-success">
+                                        <i class="fa fa-check"></i> ثبت
+                                    </button>
+
+                                    <a href="<?= $list_url."?shop=".$row->shop_id ?>"><span name="back" class="btn btn-inverse">بازگشت به لیست</span></a>
+
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <!-- ----------- end row of fields ----------------------------------------------------------- -->
+                
+            </div>
+        </div>
+    </div>
+    <!-- ----------- end main container ------------------------------------------------------------------------ -->
+</div>
+
+<? include('footer.php'); ?>
+
