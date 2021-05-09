ab8efe50bb6784370cd558fdc35a0bace785d6d9
hadiye <60326153+hadiye@users.noreply.github.com>
2021-05-09 11:25:12 +0330

    Update index.php

--- guild/index.php
+++ guild/index.php
@@ -4,7 +4,7 @@ $action = new Action();
 
 // check admin access
 if ($action->auth()) {
-    // echo "<script type='text/javascript'>window.location.href = 'panel.php';</script>";
+     echo "<script type='text/javascript'>window.location.href = 'panel.php';</script>";
     return 0;
 }
 
